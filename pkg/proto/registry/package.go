31db763810988eee73ddb4fdbd422017fbb3ebeb
micnncim <micnncim@gmail.com>
2019-09-02 18:50:35 +0900

    Update proto registry

--- pkg/proto/registry/package.go
+++ pkg/proto/registry/package.go
@@ -2,23 +2,12 @@ package registry
 
 import "github.com/emicklei/proto"
 
-type PackageSet struct {
-	packageNameToPackage map[string]*Package
-}
-
 type Package struct {
 	protoPackage *proto.Package
-
-	messageNameToMessage map[string]*Message
-	enumNameToEnum       map[string]*Enum
-	serviceNameToService map[string]*Service
 }
 
-func NewPackage(protoPackage *proto.Package) *Package {
+func newPackage(protoPackage *proto.Package) *Package {
 	return &Package{
-		protoPackage:         protoPackage,
-		messageNameToMessage: make(map[string]*Message),
-		enumNameToEnum:       make(map[string]*Enum),
-		serviceNameToService: make(map[string]*Service),
+		protoPackage: protoPackage,
 	}
 }
--- pkg/proto/registry/proto.go
+++ pkg/proto/registry/proto.go
@@ -5,10 +5,12 @@ import "github.com/emicklei/proto"
 type Proto struct {
 	protoProto *proto.Proto
 
+	packageNameToPackage map[string]*Package
 	messageNameToMessage map[string]*Message
 	enumNameToEnum       map[string]*Enum
 	serviceNameToService map[string]*Service
 
+	lineToPackage map[int]*Package
 	lineToMessage map[int]*Message
 	lineToEnum    map[int]*Enum
 	lineToService map[int]*Service
@@ -18,10 +20,12 @@ func NewProto(protoProto *proto.Proto) *Proto {
 	p := &Proto{
 		protoProto: protoProto,
 
+		packageNameToPackage: make(map[string]*Package),
 		messageNameToMessage: make(map[string]*Message),
 		enumNameToEnum:       make(map[string]*Enum),
 		serviceNameToService: make(map[string]*Service),
 
+		lineToPackage: make(map[int]*Package),
 		lineToMessage: make(map[int]*Message),
 		lineToEnum:    make(map[int]*Enum),
 		lineToService: make(map[int]*Service),
@@ -29,6 +33,11 @@ func NewProto(protoProto *proto.Proto) *Proto {
 
 	for _, el := range protoProto.Elements {
 		switch v := el.(type) {
+
+		case *proto.Package:
+			pkg := newPackage(v)
+			p.packageNameToPackage[v.Name] = pkg
+			p.lineToPackage[v.Position.Line] = pkg
 
 		case *proto.Message:
 			m := newMessage(v)
