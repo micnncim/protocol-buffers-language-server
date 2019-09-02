979154c75f7c74865ec68bb80d3380703ae62b19
micnncim <micnncim@gmail.com>
2019-09-02 22:04:40 +0900

    Fix error handling in parsing proto files

--- pkg/proto/parser/parser.go
+++ pkg/proto/parser/parser.go
@@ -4,6 +4,7 @@ import (
 	"os"
 
 	"github.com/emicklei/proto"
+	"go.uber.org/multierr"
 
 	"github.com/micnncim/protocol-buffers-language-server/pkg/proto/registry"
 )
@@ -11,18 +12,22 @@ import (
 func ParseProtos(filenames ...string) (*registry.ProtoSet, error) {
 	protoSet := registry.NewProtoSet()
 
+	var errs error
 	for _, filename := range filenames {
-		f, err := os.Open(filename)
-		if err != nil {
-			return nil, err
-		}
-		parser := proto.NewParser(f)
-		p, err := parser.Parse()
-		if err != nil {
-			return nil, err
-		}
-		protoSet.Append(p)
+		func() {
+			f, err := os.Open(filename)
+			if err != nil {
+				multierr.Append(errs, err)
+			}
+			defer f.Close()
+			parser := proto.NewParser(f)
+			p, err := parser.Parse()
+			if err != nil {
+				multierr.Append(errs, err)
+			}
+			protoSet.Append(p)
+		}()
 	}
 
-	return protoSet, nil
+	return protoSet, errs
 }
