9e46c1faa3b9f851f1670b66200321642f607dd1
micnncim <micnncim@gmail.com>
2019-09-08 23:23:06 +0900

    Remove panic from not implemented methods

--- pkg/lsp/server/server.go
+++ pkg/lsp/server/server.go
@@ -135,31 +135,38 @@ func (s *Server) Exit(ctx context.Context) (err error) {
 }
 
 func (s *Server) CodeAction(ctx context.Context, params *protocol.CodeActionParams) (result []protocol.CodeAction, err error) {
-	panic("not implement yet")
+	err = notImplemented("CodeAction")
+	return
 }
 
 func (s *Server) CodeLens(ctx context.Context, params *protocol.CodeLensParams) (result []protocol.CodeLens, err error) {
-	panic("not implement yet")
+	err = notImplemented("CodeLens")
+	return
 }
 
 func (s *Server) CodeLensResolve(ctx context.Context, params *protocol.CodeLens) (result *protocol.CodeLens, err error) {
-	panic("not implement yet")
+	err = notImplemented("CodeLensResolve")
+	return
 }
 
 func (s *Server) ColorPresentation(ctx context.Context, params *protocol.ColorPresentationParams) (result []protocol.ColorPresentation, err error) {
-	panic("not implement yet")
+	err = notImplemented("ColorPresentation")
+	return
 }
 
 func (s *Server) Completion(ctx context.Context, params *protocol.CompletionParams) (result *protocol.CompletionList, err error) {
-	panic("not implement yet")
+	err = notImplemented("Completion")
+	return
 }
 
 func (s *Server) CompletionResolve(ctx context.Context, params *protocol.CompletionItem) (result *protocol.CompletionItem, err error) {
-	panic("not implement yet")
+	err = notImplemented("CompletionResolve")
+	return
 }
 
 func (s *Server) Declaration(ctx context.Context, params *protocol.TextDocumentPositionParams) (result []protocol.Location, err error) {
-	panic("not implement yet")
+	err = notImplemented("Declaration")
+	return
 }
 
 // Definition implements textDocument/definition method.
@@ -169,15 +176,18 @@ func (s *Server) Definition(ctx context.Context, params *protocol.TextDocumentPo
 }
 
 func (s *Server) DidChange(ctx context.Context, params *protocol.DidChangeTextDocumentParams) (err error) {
-	panic("not implement yet")
+	err = notImplemented("DidChange")
+	return
 }
 
 func (s *Server) DidChangeConfiguration(ctx context.Context, params *protocol.DidChangeConfigurationParams) (err error) {
-	panic("not implement yet")
+	err = notImplemented("DidChangeConfiguration")
+	return
 }
 
 func (s *Server) DidChangeWatchedFiles(ctx context.Context, params *protocol.DidChangeWatchedFilesParams) (err error) {
-	panic("not implement yet")
+	err = notImplemented("DidChangeWatchedFiles")
+	return
 }
 
 // DidChangeWorkspaceFolders implements workspace/didChangeWorkspaceFolders method.
@@ -205,81 +215,105 @@ func (s *Server) DidSave(ctx context.Context, params *protocol.DidSaveTextDocume
 }
 
 func (s *Server) DocumentColor(ctx context.Context, params *protocol.DocumentColorParams) (result []protocol.ColorInformation, err error) {
-	panic("not implement yet")
+	err = notImplemented("DocumentColor")
+	return
 }
 
 func (s *Server) DocumentHighlight(ctx context.Context, params *protocol.TextDocumentPositionParams) (result []protocol.DocumentHighlight, err error) {
-	panic("not implement yet")
+	err = notImplemented("DocumentHighlight")
+	return
 }
 
 func (s *Server) DocumentLink(ctx context.Context, params *protocol.DocumentLinkParams) (result []protocol.DocumentLink, err error) {
-	panic("not implement yet")
+	err = notImplemented("DocumentLink")
+	return
 }
 
 func (s *Server) DocumentLinkResolve(ctx context.Context, params *protocol.DocumentLink) (result *protocol.DocumentLink, err error) {
-	panic("not implement yet")
+	err = notImplemented("DocumentLinkResolve")
+	return
 }
 
 func (s *Server) DocumentSymbol(ctx context.Context, params *protocol.DocumentSymbolParams) (result []protocol.DocumentSymbol, err error) {
-	panic("not implement yet")
+	err = notImplemented("DocumentSymbol")
+	return
 }
 
 func (s *Server) ExecuteCommand(ctx context.Context, params *protocol.ExecuteCommandParams) (result interface{}, err error) {
-	panic("not implement yet")
+	err = notImplemented("ExecuteCommand")
+	return
 }
 
 func (s *Server) FoldingRanges(ctx context.Context, params *protocol.FoldingRangeParams) (result []protocol.FoldingRange, err error) {
-	panic("not implement yet")
+	err = notImplemented("FoldingRanges")
+	return
 }
 
 func (s *Server) Formatting(ctx context.Context, params *protocol.DocumentFormattingParams) (result []protocol.TextEdit, err error) {
-	panic("not implement yet")
+	err = notImplemented("Formatting")
+	return
 }
 
 func (s *Server) Hover(ctx context.Context, params *protocol.TextDocumentPositionParams) (result *protocol.Hover, err error) {
-	panic("not implement yet")
+	err = notImplemented("Hover")
+	return
 }
 
 func (s *Server) Implementation(ctx context.Context, params *protocol.TextDocumentPositionParams) (result []protocol.Location, err error) {
-	panic("not implement yet")
+	err = notImplemented("Implementation")
+	return
 }
 
 func (s *Server) OnTypeFormatting(ctx context.Context, params *protocol.DocumentOnTypeFormattingParams) (result []protocol.TextEdit, err error) {
-	panic("not implement yet")
+	err = notImplemented("OnTypeFormatting")
+	return
 }
 
 func (s *Server) PrepareRename(ctx context.Context, params *protocol.TextDocumentPositionParams) (result *protocol.Range, err error) {
-	panic("not implement yet")
+	err = notImplemented("PrepareRename")
+	return
 }
 
 func (s *Server) RangeFormatting(ctx context.Context, params *protocol.DocumentRangeFormattingParams) (result []protocol.TextEdit, err error) {
-	panic("not implement yet")
+	err = notImplemented("RangeFormatting")
+	return
 }
 
 func (s *Server) References(ctx context.Context, params *protocol.ReferenceParams) (result []protocol.Location, err error) {
-	panic("not implement yet")
+	err = notImplemented("References")
+	return
 }
 
 func (s *Server) Rename(ctx context.Context, params *protocol.RenameParams) (result *protocol.WorkspaceEdit, err error) {
-	panic("not implement yet")
+	err = notImplemented("Rename")
+	return
 }
 
 func (s *Server) SignatureHelp(ctx context.Context, params *protocol.TextDocumentPositionParams) (result *protocol.SignatureHelp, err error) {
-	panic("not implement yet")
+	err = notImplemented("SignatureHelp")
+	return
 }
 
 func (s *Server) Symbols(ctx context.Context, params *protocol.WorkspaceSymbolParams) (result []protocol.SymbolInformation, err error) {
-	panic("not implement yet")
+	err = notImplemented("Symbols")
+	return
 }
 
 func (s *Server) TypeDefinition(ctx context.Context, params *protocol.TextDocumentPositionParams) (result []protocol.Location, err error) {
-	panic("not implement yet")
+	err = notImplemented("TypeDefinition")
+	return
 }
 
 func (s *Server) WillSave(ctx context.Context, params *protocol.WillSaveTextDocumentParams) (err error) {
-	panic("not implement yet")
+	err = notImplemented("WillSave")
+	return
 }
 
 func (s *Server) WillSaveWaitUntil(ctx context.Context, params *protocol.WillSaveTextDocumentParams) (result []protocol.TextEdit, err error) {
-	panic("not implement yet")
+	err = notImplemented("WillSaveWaitUntil")
+	return
+}
+
+func notImplemented(method string) *jsonrpc2.Error {
+	return jsonrpc2.NewError(jsonrpc2.MethodNotFound, "method %q not implemented", method)
 }
