6f2a953e72b43840a8012891e267b74f48744ad7
micnncim <micnncim@gmail.com>
2019-09-10 11:52:15 +0900

    Update parser to use io.Reader

--- pkg/proto/parser/parser.go
+++ pkg/proto/parser/parser.go
@@ -15,34 +15,19 @@
 package parser
 
 import (
-	"os"
+	"io"
 
-	"github.com/emicklei/proto"
-	"go.uber.org/multierr"
+	protobuf "github.com/emicklei/proto"
 
 	"github.com/micnncim/protocol-buffers-language-server/pkg/proto/registry"
 )
 
 // ParseProtos parses protobuf files from filenames and return registry.ProtoSet.
-func ParseProtos(filenames ...string) (registry.ProtoSet, error) {
-	protoSet := registry.NewProtoSet()
-
-	var errs error
-	for _, filename := range filenames {
-		func() {
-			f, err := os.Open(filename)
-			if err != nil {
-				multierr.Append(errs, err)
-			}
-			defer f.Close()
-			parser := proto.NewParser(f)
-			p, err := parser.Parse()
-			if err != nil {
-				multierr.Append(errs, err)
-			}
-			protoSet.Append(p)
-		}()
+func ParseProto(r io.Reader) (registry.Proto, error) {
+	parser := protobuf.NewParser(r)
+	p, err := parser.Parse()
+	if err != nil {
+		return nil, err
 	}
-
-	return protoSet, errs
+	return registry.NewProto(p), nil
 }
